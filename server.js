55d2d5de30e92892edf2b74673c8ffe69a8084d1
Kenny Tran <trank7295@gmail.com>
2019-04-27 19:07:55 -0400

    v2

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 8080;
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
+
 app.use(bodyParser.json());
 
 var exphbs = require("express-handlebars");
