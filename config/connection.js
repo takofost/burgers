29c95dc9422748bca60d02cf23613a8f5acbfc44
Kenny Tran <trank7295@gmail.com>
2019-04-27 19:30:55 -0400

    added gitignore

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Hacktivat3",
+  password: "",
   database: "burgers_db"
 });
 
